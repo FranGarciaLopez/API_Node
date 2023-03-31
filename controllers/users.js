3ee7b065802f3838b0462ce629cc7391d719f95a
Fran <frangalo34@gmail.com>
2023-03-31 20:28:08 +0200

    get user from user name

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,10 @@
 const getUser = (userId) => {
           return userDatabase[userId];
 }
+const getUserIdFromUserName = (userName) => {
+          for (let user in userDatabase) {
+                    if(userDatabase[user].userName == userName) {
+                              return userDatabase[user];
+                    }
+          }
+}
