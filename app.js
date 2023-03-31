3bf28b559f4ceaffa3a2c798a01be5b1b55438f1
Fran <frangalo34@gmail.com>
2023-03-31 20:34:01 +0200

    refactorize app file to controllers, router and tests folders

--- app.js
+++ app.js
@@ -1,55 +1,27 @@
 const express = require('express');
-const passport = require('passport')
-require('./auth')(passport);
+var bodyParser = require('body-parser')
+
+//Routes
+const authRoutes = require('./router/auth').router;
+const teamsRoutes = require('./router/team').router;
 
 const app = express();
-const port = 3004;
+app.use(bodyParser.json());
+
+const port = 3005;
 
 app.get('/', (req, res) => {
     //req es la peticion
     //res es la respuesta
-    console.log(req)
+    console.log(res)
     res.status(200).send('Hello World!');
 });
 
-app.post('/login', (req, res) => {
-    res.status(200).json(
-        {token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.zX5MPQtbjoNAS7rpsx_hI7gqGIlXOQq758dIqyBVxxY'}
-    )
-});
-
-app.post('/team/pokemons', (req, res, next) => {
-    
-}, () => {
-        //req es la peticion
-        //res es la respuesta
-        res.status(200).send('Hello World!');
-    });
-
-app.get('/team', 
-    passport.authenticate('jwt', {session:false}),
-(req, res) => {
-    //req es la peticion
-    //res es la respuesta
-    res.status(200).send('Hello World!');
-});
-
-app.delete('/team/pokemons/:pokeid', () => {
-    //req es la peticion
-    //res es la respuesta
-    res.status(200).send('Hello World!');
-});
-
-app.put('/team', () => {
-    //req es la peticion
-    //res es la respuesta
-    console.log(req)
-    res.status(200).send('Hello World!');
-});
+app.use('/auth', authRoutes);
+app.use('/teams', teamsRoutes);
 
 app.listen(port, () => {
-    console.log('Server started at port 3004');
+    console.log('Server started at port 3005');
 });
-
 
 exports.app = app;
